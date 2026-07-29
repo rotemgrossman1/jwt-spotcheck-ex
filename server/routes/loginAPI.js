e367f1999279925a51e6de52b993d60c31f98003
rotemgrossman1 <rotem.grossman@live.biu.ac.il>
2026-07-29 13:58:35 +0300

    finished step 2

--- server/routes/loginAPI.js
+++ server/routes/loginAPI.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../../fakeDB/users');
-
+const bcrypt = require('bcryptjs');
 const secretKey = 'my_secret_key';
 
+function authenticateUser(username, password){
+  const user = users.find(u=>u.username === username);
+  if(!user){
+    return null;
+  }
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
+  if(!isPasswordValid){
+    return null;
+  }
+  return {id:user.id, username: user.username}
+}
 router.post('/login', (req, res) => {
-  res.send({ msg:"logged in?" });
+  const{username, password} = req.body;
+  const user = authenticateUser(username, password)
+  if(!user){
+    return res.status(401).send({message: 'Invalid username or password'})
+  }
+  res.send({message: 'User successfully logged in' });
 });
 
 module.exports = router;
